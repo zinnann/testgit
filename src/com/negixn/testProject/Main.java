3150203c2f054a83d6f62ee6a9cc212ea71e376b
zinnann <cool.79211331626@gmail.com>
2024-04-21 18:00:01 +0300

    addend src

---
+++ src/com/negixn/testProject/Main.java
@@ -0,0 +1,8 @@
+package com.negixn.testProject;
+
+public class Main {
+    public static void main(String[] args) {
+        Person p = new Person("Alex", 22);
+        System.out.println(p);
+    }
+}
---
+++ src/com/negixn/testProject/Person.java
@@ -0,0 +1,34 @@
+package com.negixn.testProject;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person (String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
